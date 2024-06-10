3fe7d80554d242ce4d2b13b6891928473b44b684
Eunice Hernandez <euhdz8a@gmail.com>
2024-06-10 15:17:35 -0600

    Se agregan subrutas

--- src/app.js
+++ src/app.js
@@ -6,9 +6,8 @@ const PORT = 8080;
 
 //Como separamos las rutas, acá importamos los módulos y abajo en la parte de rutas usamos estos módulos:
 import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/carts.router.js";
 
-// Para reciir datos en formato json
+// Para recibir datos en formato json
 
 app.use(express.json());
 
@@ -16,30 +15,6 @@ app.use(express.urlencoded({ extended: true })); //Para analizar los datos en la
 
 //Acá usamos los módulos de rutas que creamos:
 app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
-
-//Arrays para guardar los datos:
-
-// const products = [];
-
-// const carts = [];
-
-//Rutas
-// app.get("/", (req, res) => {
-//   res.send("Hi, welcome to products app"); //OJO: ESTO SEGURO CAMBIARA
-// });
-
-//PRODUCTS
-// app.get("/api/products", (req, res) => {
-//   res.json(products);
-// });
-
-// app.post("/api/products", (req, res) => {});
-
-//CARTS
-// app.get("/api/carts", (req, res) => {
-//   res.json(carts);
-// });
 
 // Servidor escuchando
 
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,18 +1,117 @@
 import { Router } from "express";
 const router = Router();
 
+import fs from "fs/promises";
+
 //Array para guardar los datos:
 const products = [];
 
+class ProductManager {
+  static ultId = 0;
+  //Variable estatica que almacena el ultimo ID usado.
+
+  //Debe crearse desde su constructor con el elemento products, el cual será un arreglo vacío.
+  constructor(path) {
+    this.products = [];
+    this.path = path;
+  }
+
+  //Debe contar con un método “addProduct” el cual agregará un producto al arreglo de productos inicial.
+  async addProduct(
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnails
+  ) {
+    // Validamos que se agregaron todos los campos.
+    if (
+      !title ||
+      !description ||
+      !code ||
+      !price ||
+      !stock ||
+      !category ||
+      !thumbnails
+    ) {
+      console.log("All fields are mandatory");
+      return;
+    }
+
+    // Validar que no se repita el campo “code”
+    if (this.products.some((item) => item.code === code)) {
+      console.log("The code must be unique");
+      return;
+    }
+
+    //Creamos el nuevo objeto:
+
+    const newProduct = {
+      id: ++ProductManager.ultId,
+      title,
+      description,
+      code,
+      price,
+      status: true,
+      stock,
+      category,
+      thumbnails,
+    };
+
+    //Lo agrego al array:
+    this.products.push(newProduct);
+
+    //Nuevo pasito: lo guardo en el archivo:
+    await this.saveArchivo(this.products);
+  }
+
+  //Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento
+  async getProducts() {
+    let arrayProducts = await this.readArchivo();
+    return arrayProducts;
+  }
+
+  //Debe contar con un método “getProductById” el cual debe buscar en el arreglo el producto que coincida con el id
+  getProductById(id) {
+    const product = this.products.find((item) => item.id === id);
+
+    if (!product) {
+      console.error("Not Found");
+    } else {
+      console.log("Found product:", product);
+    }
+  }
+
+  //Métodos auxiliares para guardarArchivo y leerArchivo:
+  async saveArchivo(arrayProducts) {
+    try {
+      await fs.writeFile(this.path, JSON.stringify(arrayProducts, null, 2));
+    } catch (error) {
+      console.log("Error saving file: ", error);
+    }
+  }
+
+  async readArchivo() {
+    try {
+      const response = await fs.readFile(this.path, "utf-8");
+      const array = JSON.parse(response);
+      return array;
+    } catch (error) {
+      console.log("Error reading file: ", error);
+    }
+  }
+}
+
 // Para pedir el listado de productos que tiene mi sistema.
 //OJO:antes de get va el nombre del archivo en que estamos
-//originalmente estaba "/api.pets", pero pa simplificar se deja "/" y en app.js se pone app.use("/api.pets", petsRouter)
 router.get("/", (req, res) => {
   res.json(products); //aquí estamos pidiendo el array de productos. Lo vemos en el navegador
 });
 
-// Para cargar una mascota usamos el método POST. OJO:antes de post va el nombre del archivo en que estamos
-//originalmente estaba "/api.pets", pero pa simplificar se deja "/" y en app.js se pone app.use("/api.pets", petsRouter)
+// Para cargar un producto usamos el método POST. OJO:antes de post va el nombre del archivo en que estamos
 router.post("/", (req, res) => {
   const newProduct = req.body;
   products.push(newProduct);
